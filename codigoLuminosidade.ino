c16bf03a510a57238998cb1459561060e523033f
Lucas Bonato <lucas.perez.bonato@gmail.com>
2025-04-25 15:15:05 -0300

    [REFACTOR] Create a method to turn on and off the leds

--- codigoLuminosidade.ino
+++ codigoLuminosidade.ino
@@ -1,8 +1,11 @@
 #define LDR_PIN 2
+
 #define BUZZER_PIN 13
+
 #define LED_GREEN_PIN 12
 #define LED_YELLOW_PIN 11
 #define LED_RED_PIN 10
+
 #define LDR_ANALOG_PORT A0
 
 bool isBuzzerActive = false;
@@ -10,9 +13,6 @@ long buzzerTime = 0;
 
 const float OK = 88;
 const float WARNING = 176;
-
-const float GAMMA = 0.7;
-const float RL10 = 50;
 
 const long buzzerIntervalDuration = 3000;
 
@@ -34,19 +34,13 @@ void loop() {
     Serial.println(lux);
 
     if (lux < OK) {
-        digitalWrite(LED_GREEN_PIN, HIGH);
-
-        digitalWrite(LED_YELLOW_PIN, LOW);
-        digitalWrite(LED_RED_PIN, LOW);
+        turnPins(HIGH, LOW, LOW);
         digitalWrite(BUZZER_PIN, LOW);
         isBuzzerActive = false;
     }
     else if (lux < WARNING) {
-        digitalWrite(LED_YELLOW_PIN, HIGH);
+        turnPins(LOW, HIGH, LOW);
 
-        digitalWrite(LED_GREEN_PIN, LOW);
-        digitalWrite(LED_RED_PIN, LOW);
-        
         if (isBuzzerActive && (currentTime - buzzerTime >= buzzerIntervalDuration)) {
             buzzerTime = currentTime;
             digitalWrite(BUZZER_PIN, LOW);
@@ -59,10 +53,7 @@ void loop() {
         }
     }
     else {
-        digitalWrite(LED_RED_PIN, HIGH);
-
-        digitalWrite(LED_GREEN_PIN, LOW);
-        digitalWrite(LED_YELLOW_PIN, LOW);
+        turnPins(LOW, LOW, HIGH);
         digitalWrite(BUZZER_PIN, LOW);
       isBuzzerActive = false;
     }
@@ -70,7 +61,15 @@ void loop() {
     delay(100);
 }
 
+void turnPins(int green, int yellow, int red) {
+    digitalWrite(LED_GREEN_PIN, green);
+    digitalWrite(LED_YELLOW_PIN, yellow);
+    digitalWrite(LED_RED_PIN, red);
+}
+
 float getLuxValue(int analogValue) {
+    const float GAMMA = 0.7;
+    const float RL10 = 50;
     float voltage = analogValue / 1024. * 5;
     float resistance = 2000 * voltage / (1 - voltage / 5);
     float lux = pow(RL10 * 1e3 * pow(10, GAMMA) / resistance, (1 / GAMMA));
